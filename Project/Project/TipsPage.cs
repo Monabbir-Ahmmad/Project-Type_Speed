ded786653155f08e19f77a0ac18f5b96647cb878
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2019-10-30 01:05:05 +0600

    Added project

---
+++ Project/Project/TipsPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class TipsPage : UserControl
+    {
+        public TipsPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void TipsPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TipsTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
